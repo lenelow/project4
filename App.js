a35741fbb87760517d7c7e68e199977f6c231b86
Leah Enelow <lenelow@students.terpmail.edu>
2018-07-31 18:43:59 -0400

    finished navigation in app.js

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { createStackNavigator } from "react-navigation";
 import HomePage from "./components/HomePage";
 import HouseIndex from "./components/HouseIndex";
+import House from "./components/House";
 
 const App = createStackNavigator({
   Home: { screen: HomePage },
-  Houses: { screen: HouseIndex }
+  Houses: { screen: HouseIndex },
+  House: { screen: House },
+  Character: { screen: Character }
 });
 
 export default App;
